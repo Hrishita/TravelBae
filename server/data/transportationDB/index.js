a7e2f1379ea18bff84e8bffe0a5808b44d60e815
mavani <hr637632@dal.ca>
2022-07-09 17:44:53 -0300

    Bug fix in transportation api

--- server/data/transportationDB/index.js
+++ server/data/transportationDB/index.js
@@ -14,7 +14,105 @@ exports.fetchAllTransportation = function (req, res) {
 };
 
 exports.searchTransportation = function (req, res) {
+   // if both source and destination are entered
+  if (req.body.source && req.body.dest_name) {
+    Transportation.find(
+      {
+        source: req.body.source,
+        dest_name: req.body.dest_name,
+      },
+      function (err, Transportation) {
+        if (err) return res.json({ success: false, error: err });
+        res.json({ data: Transportation });
+      }
+    );
+  }
+  // if only source is entered
+  else if (req.body.source) {
+    Transportation.find(
+      {
+        source: req.body.source,
+      },
+      function (err, Transportation) {
+        if (err) return res.json({ success: false, error: err });
+        res.json({ data: Transportation });
+      }
+    );
+  }
+  // if only destination is entered
+  else if (req.body.dest_name) {
+    Transportation.find(
+      {
+        dest_name: req.body.dest_name,
+      },
+      function (err, Transportation) {
+        if (err) return res.json({ success: false, error: err });
+        res.json({ data: Transportation });
+      }
+    );
+  }
+  // if only start date is entered
+  else if(req.body.start_date){
+    Transportation.find(
+      {
+        start_date: req.body.start_date,
+      },
+      function (err, Transportation) {
+        if (err) return res.json({ success: false, error: err });
+        res.json({ data: Transportation });
+      }
+    );
+  }
+  // if only return date is entered
+  else if(req.body.return_date){
+    Transportation.find(
+      {
+        return_date: req.body.return_date,
+      },
+      function (err, Transportation) {
+        if (err) return res.json({ success: false, error: err });
+        res.json({ data: Transportation });
+      }
+    );
+  }
+  // if both start date and return date are entered
+  else if(req.body.start_date && req.body.return_date){
+    Transportation.find(
+      {
+        start_date: req.body.start_date,
+        return_date: req.body.return_date,
+      },
+      function (err, Transportation) {
+        if (err) return res.json({ success: false, error: err });
+        res.json({ data: Transportation });
+      }
+    );
+  }
+      // if both source and destination are entered and start date is entered
+
+  else if(req.body.source && req.body.dest_name && req.body.start_date && req.body.return_date){
+    Transportation.find(
+      {
+        source: req.body.source,
+        dest_name: req.body.dest_name,
+        start_date: req.body.start_date,
+        return_date: req.body.return_date,
+      },
+      function (err, Transportation) {
+        if (err) return res.json({ success: false, error: err });
+        res.json({ data: Transportation });
+      }
+    );
+  }
+else{
   Transportation.find(  
+    // if only destination is entered 
+   
+    // if only source is entered
+    // if only start date is entered
+    // if only return date is entered
+    // if both start date and return date are entered
+
     { $or:[{source: req.body.source}, {dest_name: req.body.dest_name},{start_date: req.body.start_date},{return_date:req.body.return_date}] },
     function (err, transportation) {
       if (err) return res.json({ success: false, error: err });
@@ -22,3 +120,4 @@ exports.searchTransportation = function (req, res) {
     }
   );
 };
+}
