6adee8feb7732c9dca5ef2393f38c6e3f6b2a6a3
Nishit Mistry <ns209086@dal.ca>
2022-07-15 14:57:37 -0300

    bug fixes

--- client/src/pages/SearchFlights.js
+++ client/src/pages/SearchFlights.js
@@ -1,5 +1,4 @@
 import { Box, ButtonBase, Pagination, Stack } from "@mui/material";
-
 import { Grid } from "@mui/material";
 import Footer from "../containers/Footer";
 import { LocationCity } from "@mui/icons-material";
@@ -17,7 +16,7 @@ import {
   Typography,
 } from "@mui/material";
 import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import NavBar from "../containers/NavBar";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 import { deepOrange, grey } from "@mui/material/colors";
@@ -30,16 +29,16 @@ import { AuthContext } from "../context/AuthContext";
 
 function SearchFlights() {
   const [startDate, setStartDate] = useState();
-  const [source, setSource] = useState('');
-  const [dest, setDest] = useState('');
+  const [source, setSource] = useState("");
+  const [dest, setDest] = useState("");
   const [endDate, setEndDate] = useState();
   const [tripType, setTripType] = useState(0);
   const [showSnackbar, setShowSnackbar] = useState(false);
 
-  const [flights, setFlights] = useState([])
-  const [filteredFlights, setFilteredFlights] = useState([])
-  const [price, setPrice] = useState()
-  const [show, setShow] = useState(false)
+  const [flights, setFlights] = useState([]);
+  const [filteredFlights, setFilteredFlights] = useState([]);
+  const [price, setPrice] = useState();
+  const [show, setShow] = useState(false);
   const auth = useContext(AuthContext);
   const userId = auth.userId ? auth.userId : "";
   const fetchRecommendedFlights = async () => {
@@ -47,88 +46,92 @@ function SearchFlights() {
       method: "POST",
 
       url: `${BACKEND_URL}/tp/fetchAllTransporation`,
-    })
-    setFlights(res.data.data)
-    setFilteredFlights(res.data.data)
-  }
- 
+    });
+    setFlights(res.data.data);
+    setFilteredFlights(res.data.data);
+  };
+
   useEffect(() => {
-    fetchRecommendedFlights()
-  }, [])
+    fetchRecommendedFlights();
+  }, []);
 
   const checkForEmptySearch = (source, dest) => {
-    console.log(source, dest)
-    if (source == '' && dest == '') {
-      fetchRecommendedFlights()
+    console.log(source, dest);
+    if (source == "" && dest == "") {
+      fetchRecommendedFlights();
     }
-  }
+  };
 
   const onShowSnackbar = () => {
     setShowSnackbar(true);
   };
 
   const handleSearch = async () => {
-    if (source == '' || dest == '') {
-      setShow(true)
+    if (source == "" || dest == "") {
+      setShow(true);
     } else {
-      if (tripType == 0) { //one way
-        let d = new Date(startDate)
-        d.setHours(0)
-        d.setSeconds(0)
-        d.setMinutes(0)
+      if (tripType == 0) {
+        //one way
+        let d = new Date(startDate);
+        d.setHours(0);
+        d.setSeconds(0);
+        d.setMinutes(0);
         let res = await axios({
-          method: 'POST',
+          method: "POST",
           url: `${BACKEND_URL}/tp/fetchTransporationByDestination`,
           data: {
             source: source ? source : null,
             dest_name: dest ? dest : null,
             start_date: startDate ? d.getTime() : null,
-          }
-        })
-        setFlights(res.data.data)
-        setFilteredFlights(res.data.data) 
-      } else { //round trip
-        let d1 = new Date(startDate)
-        d1.setHours(0)
-        d1.setSeconds(0)
-        d1.setMinutes(0)
-        let d2 = new Date(endDate)
-        d2.setHours(0)
-        d2.setSeconds(0)
-        d2.setMinutes(0)
-        console.log(d1.getTime(), d2.getTime())
+          },
+        });
+        setFlights(res.data.data);
+        setFilteredFlights(res.data.data);
+      } else {
+        //round trip
+        let d1 = new Date(startDate);
+        d1.setHours(0);
+        d1.setSeconds(0);
+        d1.setMinutes(0);
+        let d2 = new Date(endDate);
+        d2.setHours(0);
+        d2.setSeconds(0);
+        d2.setMinutes(0);
+        console.log(d1.getTime(), d2.getTime());
         let res = await axios({
-          method: 'POST',
+          method: "POST",
           url: `${BACKEND_URL}/tp/fetchTransporationByDestination`,
           data: {
             source: source ? source : null,
             dest_name: dest ? dest : null,
             start_date: startDate ? d1.getTime() : null,
             return_date: endDate ? d2.getTime() : null,
-          }
-        })
-        setFlights(res.data.data)
-        setFilteredFlights(res.data.data)
+          },
+        });
+        setFlights(res.data.data);
+        setFilteredFlights(res.data.data);
       }
     }
   };
 
   const handleSort = () => {
-    let sorted = [...flights]
-    sorted.sort((a, b) => {return a.price-b.price})
-    setFilteredFlights(sorted)
-    console.log(sorted)
-  }
+    let sorted = [...flights];
+    sorted.sort((a, b) => {
+      return a.price - b.price;
+    });
+    setFilteredFlights(sorted);
+    console.log(sorted);
+  };
 
   const handlePriceChange = (price) => {
-    setPrice(price)
-    let newFlightsList = [...flights]
-    newFlightsList = newFlightsList.filter(f => {
-      console.log(f.price, price)
-      return f.price <= price
-    })
-    setFilteredFlights(newFlightsList)
-  }
+    setPrice(price);
+    let newFlightsList = [...flights];
+    newFlightsList = newFlightsList.filter((f) => {
+      console.log(f.price, price);
+      return f.price <= price;
+    });
+    setFilteredFlights(newFlightsList);
+  };
 
   const [page, setPage] = useState(1);
   const PER_PAGE = 9;
@@ -143,29 +146,40 @@ function SearchFlights() {
     let res = await axios({
       method: "POST",
       url: `${BACKEND_URL}/pt/createPlanTrip`,
-      transportation: {...data, plan_id: Math.random() * 1000000, city: data.dest_name, emailid: userID, country: "temp"},
-    })
-    console.log("response", res)
-  }
+      transportation: {
+        ...data,
+        plan_id: Math.random() * 1000000,
+        city: data.dest_name,
+        emailid: userId,
+        country: "temp",
+      },
+    });
+    console.log("response", res);
+  };
 
   const _DATA = usePagination(filteredFlights, PER_PAGE);
 
   return (
     <Grid container>
-      {
-        show ? <ModalComp
+      {show ? (
+        <ModalComp
           show={show}
-          handleClose={() => {setShow(false)}}
-          text={'Please enter both source and destination place'}
-        /> : null
-      }
+          handleClose={() => {
+            setShow(false);
+          }}
+          text={"Please enter both source and destination place"}
+        />
+      ) : null}
       <Grid item xs={12}>
         <NavBar />
       </Grid>
       <Grid item xs={12}>
         <Grid container>
           <Grid item xs={12}>
-            <Box component={'div'} className="card shadow flex-wrap p-3 d-flex justify-content-start align-items-center flex-row my-3">
+            <Box
+              component={"div"}
+              className="card shadow flex-wrap p-3 d-flex justify-content-start align-items-center flex-row my-3"
+            >
               <Snackbar
                 open={showSnackbar}
                 autoHideDuration={6000}
@@ -206,8 +220,10 @@ function SearchFlights() {
                 <TextField
                   id="input-with-sx"
                   value={source}
-                  onChange={(e) => {setSource(e.target.value); 
-                    checkForEmptySearch(e.target.value, dest) } }
+                  onChange={(e) => {
+                    setSource(e.target.value);
+                    checkForEmptySearch(e.target.value, dest);
+                  }}
                   label="Where From?"
                   variant="standard"
                 />
@@ -220,8 +236,10 @@ function SearchFlights() {
                   id="input-with-sx"
                   label="Where To?"
                   value={dest}
-                  onChange={(e) => {setDest(e.target.value) 
-                    checkForEmptySearch(source, e.target.value) } }
+                  onChange={(e) => {
+                    setDest(e.target.value);
+                    checkForEmptySearch(source, e.target.value);
+                  }}
                   variant="standard"
                 />
               </Box>
@@ -252,7 +270,7 @@ function SearchFlights() {
                 </Box>
               ) : null}
 
-              <Box component={'div'} style={{ flex: 1, flexBasis: "100%" }}>
+              <Box component={"div"} style={{ flex: 1, flexBasis: "100%" }}>
                 <Typography id="non-linear-slider" gutterBottom>
                   Price
                 </Typography>
@@ -260,13 +278,12 @@ function SearchFlights() {
                   defaultValue={3000}
                   aria-label="Default"
                   valueLabelDisplay="auto"
-                  value={price} 
-                  onChange={(e)=>{
-                    handlePriceChange(e.target.value)
-                  }} 
+                  value={price}
+                  onChange={(e) => {
+                    handlePriceChange(e.target.value);
+                  }}
                   min={1000}
                   max={10000}
-                  
                 />
               </Box>
               <Button
@@ -278,9 +295,13 @@ function SearchFlights() {
               </Button>
             </Box>
 
-            <Box component={'div'} className="card shadow my-3">
-              <Box className='d-flex flex-row justify-content-start align-items-center'>
-                <Typography className="mx-4 my-3" variant="h4" fontWeight={"600"}>
+            <Box component={"div"} className="card shadow my-3">
+              <Box className="d-flex flex-row justify-content-start align-items-center">
+                <Typography
+                  className="mx-4 my-3"
+                  variant="h4"
+                  fontWeight={"600"}
+                >
                   All flights
                 </Typography>
                 <ButtonBase>
@@ -294,31 +315,32 @@ function SearchFlights() {
                     className="mx-2 my-2 d-flex flex-row justify-content-between align-items-center flex-wrap"
                     style={{ cursor: "pointer" }}
                   >
-                    <Box component={'div'} className="d-flex flex-row justify-content-between align-items-center py-2">
+                    <Box
+                      component={"div"}
+                      className="d-flex flex-row justify-content-between align-items-center py-2"
+                    >
                       <Avatar sx={{ bgcolor: deepOrange[500], mx: 1.4 }}>
-                      {d.flight_company_logo}
+                        {d.flight_company_logo}
                       </Avatar>
-                      <Box component={'div'} className="d-flex flex-column justify-content-center align-items-center mx-2">
-                        <Typography variant={"h5"}>
-                          {" "}
-                          {d.source}
-                        </Typography>
+                      <Box
+                        component={"div"}
+                        className="d-flex flex-column justify-content-center align-items-center mx-2"
+                      >
+                        <Typography variant={"h5"}> {d.source}</Typography>
                         <Typography variant={"body1"}>
                           {" "}
                           {new Date(d.start_date).toLocaleString()}
                         </Typography>
-                   
                       </Box>
-                      <Box component={'div'} className='mx-3'>
-                        <Box component={'div'}>
-                          {d.trip_duration}
-                        </Box>
-                        <Box>
-                          &lt;---------------------------&gt;
-                        </Box>
+                      <Box component={"div"} className="mx-3">
+                        <Box component={"div"}>{d.trip_duration}</Box>
+                        <Box>&lt;---------------------------&gt;</Box>
                       </Box>
-                      <Box component={'div'} className="d-flex flex-column justify-content-center align-items-center mx-2">
-                        <Typography variant={"h5"} >
+                      <Box
+                        component={"div"}
+                        className="d-flex flex-column justify-content-center align-items-center mx-2"
+                      >
+                        <Typography variant={"h5"}>
                           {" "}
                           {d.dest_name}
                           {/* {d.startTime} - {d.endTime}{" "} */}
@@ -327,11 +349,10 @@ function SearchFlights() {
                           {" "}
                           {new Date(d.return_date).toLocaleString()}
                         </Typography>
-          
                       </Box>
                     </Box>
 
-                    <Box component={'div'} className="mx-4 py-2">
+                    <Box component={"div"} className="mx-4 py-2">
                       <Typography variant="body1" color={grey[700]}>
                         {d.flight_company}
                       </Typography>
@@ -343,8 +364,14 @@ function SearchFlights() {
                       </Typography>
                     </Box>
 
-                    <Box component={'div'} className='mx-4 py-2'>
-                      <Button onClick={() => {planTripHandler(d)}} style={{zIndex: 99}} variant={'contained'}>
+                    <Box component={"div"} className="mx-4 py-2">
+                      <Button
+                        onClick={() => {
+                          planTripHandler(d);
+                        }}
+                        style={{ zIndex: 99 }}
+                        variant={"contained"}
+                      >
                         Add to planned trip
                       </Button>
                     </Box>
@@ -353,21 +380,21 @@ function SearchFlights() {
               })}
             </Box>
             <Grid item xs={12}>
-            <Grid container alignItems="center" justifyContent="center">
-              <Grid container justifyContent="center" sx={{ mt: 3, mb: 2 }}>
-                <Stack spacing={2}>
-                  <Pagination
-                    count={count}
-                    color="primary"
-                    page={page}
-                    variant="outlined"
-                    shape="rounded"
-                    onChange={handleChange1}
-                  />
-                </Stack>
+              <Grid container alignItems="center" justifyContent="center">
+                <Grid container justifyContent="center" sx={{ mt: 3, mb: 2 }}>
+                  <Stack spacing={2}>
+                    <Pagination
+                      count={count}
+                      color="primary"
+                      page={page}
+                      variant="outlined"
+                      shape="rounded"
+                      onChange={handleChange1}
+                    />
+                  </Stack>
+                </Grid>
               </Grid>
             </Grid>
-          </Grid>
           </Grid>
         </Grid>
         <Grid item xs={12}>
